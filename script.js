e23dcea0d1d7bb4e1c4e1748c567401a017ae991
daniela00108 <daniela.koka@inbox.lv>
2026-04-21 10:09:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,72 @@
 const datums = new Date();
 izvade.innerHTML = datums;
+
+function pievienot() {
+    let prieksmets = document.getElementById("Priekšmets").value
+    let datums = document.getElementById("Datums").value
+    let uzdevums = document.getElementById("Uzdevums").value
+
+    if(!prieksmets || !datums || !uzdevums){
+        alert("Aizpildiet visus laukus!")
+        return;
+    }
+    let saraksts = JSON.parse(localStorage.getItem("uzdevumi")) || []
+    saraksts.push({
+        prieksmets,
+        datums,
+        uzdevums
+    })
+
+    localStorage.setItem("uzdevumi",JSON.stringify(saraksts))
+
+    attelot()
+
+    document.getElementById("Priekšmets").value = ""
+    document.getElementById("Datums").value = ""
+    document.getElementById("Uzdevums").value = ""
+}
+
+function attelot() {
+    let saraksts = JSON.parse(localStorage.getItem("uzdevumi")) || []
+    let ul = document.getElementById("saraksts")
+
+    ul.innerHTML = " "
+
+    saraksts.forEach((item, index) => {
+        let li = document.createElement("li")
+
+        let checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        checkbox.checked = item.pabeigts
+
+        checkbox.onchange = function () {
+            saraksts[index].pabeigts = checkbox.checked
+            localStorage.setItem("uzdevumi", JSON.stringify(saraksts))
+
+        };
+        let teksts = document.createElement("span")
+        teksts.textContent =  " " + item.prieksmets + " | " + item.datums + " | " + item.uzdevums + " "
+
+        let btn = document.createElement("button")
+        btn.textContent = "Dzēst"
+        btn.onclick = function () { 
+            saraksts.splice(index, 1)
+            localStorage.setItem("uzdevumi",JSON.stringify(saraksts))
+            attelot()
+        } ;
+
+        li.appendChild(checkbox)
+        li.appendChild(teksts)
+        li.appendChild(btn)
+
+        ul.appendChild(li)
+
+        })
+}
+
+
+
+window.onload = function () {
+    attelot()
+
+}
